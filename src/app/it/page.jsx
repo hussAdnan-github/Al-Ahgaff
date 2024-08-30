b79ec0ffb13dafd2f17721b322e7cfee812d3576
hussein <hussbelf@gmail.com>
2024-08-30 18:53:45 +0300

    correct error

--- src/app/it/page.jsx
+++ src/app/it/page.jsx
@@ -33,7 +33,7 @@ export default function page() {
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
     {
   students.map((item)=>(
-    <Card name={item.name} img={item.img}/>
+    <Card key={item.id} name={item.name} img={item.img}/>
   ))
 }
     </div>
